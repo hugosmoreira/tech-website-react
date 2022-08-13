fa61db05bbd86135b02a51c31b271266c360cf5e
hugosmoreira <hugo.n4@gmail.com>
2022-08-12 20:02:35 -0700

    boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import styles from './style'
+
+function App() {
+  
+
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.padding} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>\
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
