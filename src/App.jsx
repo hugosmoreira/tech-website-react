178e2c9fd1210d24cd1c7d548ff0f6649c935a94
hugosmoreira <hugo.n4@gmail.com>
2022-08-13 16:53:01 -0700

    components set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import styles from './style'
 
+import {Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components'
+
+
 function App() {
   
 
@@ -7,13 +10,27 @@ function App() {
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.padding} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>\
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
+        </div>
+
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>\
+        <div className={`${styles.boxWidth}`}>
+          <Stats /> 
+          <Business /> 
+          <Billing /> 
+          <CardDeal/> 
+          <Testimonials /> 
+          <Clients /> 
+          <CTA /> 
+          <Footer />
         </div>
 
       </div>
