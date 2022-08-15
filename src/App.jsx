29425f0ed8c719c70a587d9bac4010b2b1b26f52
hugosmoreira <hugo.n4@gmail.com>
2022-08-14 20:08:33 -0700

    navbar completo e inicio do hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import styles from './style'
+import styles from "./style";
 
 import {Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components'
 
@@ -8,11 +8,11 @@ function App() {
 
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <div className={`${styles.padding} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Navbar />
-        </div>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
       </div>
+    </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>\
         <div className={`${styles.boxWidth}`}>
--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,22 @@ import Billing from "./Billing";
 import Button from "./Button";
 import Clients from "./Clients";
 import Feedback from "./Feedback";
-import Footer from "./Footer";
 import GetStarted from "./GetStarted";
 import Stats from "./Stats";
 import Testimonials from "./Testimonials";
+import CTA from "./CTA";
+import CardDeal from "./CardDeal";
+import Hero from "./Hero";
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+  };
